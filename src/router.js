6827e3198eafb271184389aff49f875cb9b9376e
Clemens Damke <admin@cortys.de>
2015-11-24 01:44:39 +0100

    0.9.1: added support for event filters

--- src/router.js
+++ src/router.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const owe = require("owe-core");
+const filter = require("owe-helpers").filter;
 const expose = require("./expose");
 
 const connector = require("./connector");
 
-function eventRouter() {
+function eventRouter(options) {
+
+	options = {
+		filter: "filter" in options ? options.filter : false,
+		filterInverse: !!options.filterInverse || false
+	};
+
 	return function servedEventRouter(route) {
 		if(!owe.client.isApi(this.origin.eventsApi))
 			throw expose(new Error(`Events cannot be accessed via this protocol.`));
@@ -15,6 +22,18 @@ function eventRouter() {
 				closer: data => {
 					if(!data || typeof data !== "object")
 						throw expose(new TypeError(`Invalid ${route} request.`));
+
+					if("event" in data)
+						return filter(this, data.event, options.filter, Promise.resolve).then(result => {
+							if(result !== options.filterInverse)
+								return connector[route](
+									this.origin.eventsApi,
+									this.value,
+									data
+								);
+
+							throw expose(new Error(`The event ${data.event} is not exposed.`));
+						});
 
 					return connector[route](
 						this.origin.eventsApi,
